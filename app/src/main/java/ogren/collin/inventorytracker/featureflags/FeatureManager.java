50e1217c82a0a6b2139da45c159283ce8ebb1855
collinogren <collinogren@gmail.com>
2026-03-17 20:54:23 -0500

    Create simple feature flag management system and create a Snowflake database feature flag

---
+++ app/src/main/java/ogren/collin/inventorytracker/featureflags/FeatureManager.java
@@ -0,0 +1,8 @@
+package ogren.collin.inventorytracker.featureflags;
+
+public class FeatureManager {
+
+    public static final String[] ENABLED_FEATURES = {
+            Features.SNOWFLAKE_DATABASE,
+    };
+}
---
+++ app/src/main/java/ogren/collin/inventorytracker/featureflags/Features.java
@@ -0,0 +1,5 @@
+package ogren.collin.inventorytracker.featureflags;
+
+public class Features {
+    public static final String SNOWFLAKE_DATABASE = "SnowflakeDatabase";
+}
