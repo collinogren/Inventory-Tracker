a60137df71a7529c498571482ac4d4fdc0de6cd5
collinogren <collinogren@gmail.com>
2026-03-22 22:28:23 -0500

    Improve unit tests

--- app/src/androidTest/java/ogren/collin/inventorytracker/database/InventoryDatabaseTest.java
+++ app/src/androidTest/java/ogren/collin/inventorytracker/database/InventoryDatabaseTest.java
@@ -1,5 +1,8 @@
 package ogren.collin.inventorytracker.database;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import android.content.Context;
 
 import androidx.room.Room;
@@ -11,7 +14,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import ogren.collin.inventorytracker.models.Inventory.ItemType;
 import ogren.collin.inventorytracker.models.Inventory.ItemTypeDao;
+import ogren.collin.inventorytracker.models.User.User;
 import ogren.collin.inventorytracker.models.User.UserDao;
 
 @RunWith(AndroidJUnit4.class)
@@ -33,5 +38,65 @@ public class InventoryDatabaseTest {
         inventoryDatabase.close();
     }
 
-    
+    @Test
+    public void write_user_and_read() {
+        // Arrange
+        final User user = new User("username", "password");
+
+        // Act
+        userDao.insert(user);
+        final User retrievedUser = userDao.findByName("username", "password");
+
+        // Assert
+        assertThat(retrievedUser.getUsername(), equalTo(user.getUsername()));
+    }
+
+    @Test
+    public void delete_user() {
+        // Arrange
+        final User user = new User("username", "password");
+        userDao.insert(user);
+        final User retrievedUser = userDao.findByName(user.getUsername(), user.getPassword());
+
+        // Act
+        userDao.delete(retrievedUser);
+        final User retrievedUserAfterDelete = userDao.findByName("username", "password");
+
+        // Assert
+        assertThat(retrievedUserAfterDelete, equalTo(null));
+    }
+
+    @Test
+    public void write_item_and_read() {
+        // Arrange
+        final User user = new User("username", "password");
+        userDao.insert(user);
+        final User retrievedUser = userDao.findByName(user.getUsername(), user.getPassword());
+        ItemType itemType = new ItemType(1, retrievedUser.getId(), "Item Name", 10);
+
+        // Act
+        inventoryDao.insert(itemType);
+        final ItemType retrievedItemType = inventoryDao.getById(itemType.getId(), retrievedUser.getId());
+
+        // Assert
+        assertThat(retrievedItemType.getName(), equalTo(itemType.getName()));
+    }
+
+    @Test
+    public void delete_item() {
+        // Arrange
+        final User user = new User("username", "password");
+        userDao.insert(user);
+        final User retrievedUser = userDao.findByName(user.getUsername(), user.getPassword());
+
+        final ItemType itemType = new ItemType(2, retrievedUser.getId(), "Item Name", 10);
+        inventoryDao.insert(itemType);
+
+        // Act
+        inventoryDao.delete(itemType);
+        final ItemType retrievedItemType = inventoryDao.getById(itemType.getId(), retrievedUser.getId());
+
+        // Assert
+        assertThat(retrievedItemType, equalTo(null));
+    }
 }
